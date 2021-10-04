c204ecf95e1e68229dd9ebc62a9716dd1123c622
Leszek <leslawkula@gmail.com>
2021-10-04 22:51:21 +0200

    add contact form scripts to it repo

---
+++ send_form.php
@@ -0,0 +1,25 @@
+<?php
+header("content-type: application/json; charset=utf-8");
+
+$name = isset($_POST['name']) ? $_POST['name'] : "";
+$email = isset($_POST['email']) ? $_POST['email'] : "";
+$message = isset($_POST['message']) ? $_POST['message'] : "";
+
+if ($name && $email && $message) {
+    $headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=utf-8\r\nContent-Transfer-Encoding: 8bit";
+    $message_body = "Formularz kontaktowy wysłany ze strony www.kula.wroclaw.pl\n";
+    $message_body .= "Imię: $name\n";
+    $message_body .= "Adres email: $email\n\n";
+    $message_body .= $message;
+    if (mail("it1@kula.opole.pl", "Formularz kontaktowy", $message_body, $headers)) {
+        $json = array("status" => 1, "msg" => "<b>Twój formularz został pomyślnie wysłany.</b>");
+    } else {
+        $json = array("status" => 0, "msg" => "<b>Wystąpił problem z wysłaniem formularza.</b>");
+    }
+} else {
+    $json = array("status" => 0, "msg" => "<b>Proszę wypełnić wszystkie pola przed wysłaniem.</b>");
+}
+
+echo json_encode($json);
+
+exit;

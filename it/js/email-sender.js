c90f581c2e29e7b224a55683dbcb12f9e758097f
Leszek <leslawkula@gmail.com>
2021-10-10 09:06:18 +0200

    fix send_form.php

--- it/js/email-sender.js
+++ it/js/email-sender.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    $("html").on("submit", "#contact_form", function (e) {
-      e.preventDefault();
-      $("#send_form_status").html("").hide();
-      var data = $("#contact_form").serialize();
-      $.post("../send_form.php", data, function (res) {
-        $("#send_form_status").html(res.msg).show("slow");
-        if (res.status == 1) {
-          $("#contact_form")[0].reset();
-        }
-      });
+  $("html").on("submit", "#contact_form", function (e) {
+    e.preventDefault();
+    $("#send_form_status").html("").hide();
+    var data = $("#contact_form").serialize();
+    $.post("send_form.php", data, function (res) {
+      $("#send_form_status").html(res.msg).show("slow");
+      if (res.status == 1) {
+        $("#contact_form")[0].reset();
+      }
     });
   });
+});
--- it/send_form.php
+++ it/send_form.php
@@ -2,16 +2,19 @@
 header("content-type: application/json; charset=utf-8");
 
 $name = isset($_POST['name']) ? $_POST['name'] : "";
+$title = isset($_POST['title']) ? $_POST['title'] : "";
 $email = isset($_POST['email']) ? $_POST['email'] : "";
+$phone = isset($_POST['phone']) ? $_POST['phone'] : "";
 $message = isset($_POST['message']) ? $_POST['message'] : "";
 
-if ($name && $email && $message) {
+if ($name && $email && $message && $title && $phone) {
     $headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=utf-8\r\nContent-Transfer-Encoding: 8bit";
-    $message_body = "Formularz kontaktowy wysłany ze strony www.kula.wroclaw.pl\n";
+    $message_body = "Formularz kontaktowy wysłany ze strony www.kula.wroclaw.pl/it\n";
     $message_body .= "Imię: $name\n";
-    $message_body .= "Adres email: $email\n\n";
+    $message_body .= "Adres email: $email\n";
+    $message_body .= "Numer telefonu: $phone\n\n";
     $message_body .= $message;
-    if (mail("it1@kula.opole.pl", "Formularz kontaktowy", $message_body, $headers)) {
+    if (mail("it1@kula.opole.pl", $title, $message_body, $headers)) {
         $json = array("status" => 1, "msg" => "<b>Twój formularz został pomyślnie wysłany.</b>");
     } else {
         $json = array("status" => 0, "msg" => "<b>Wystąpił problem z wysłaniem formularza.</b>");
